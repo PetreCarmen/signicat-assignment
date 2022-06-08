ebf5525be7a05e893f0ca9edecb5ac1e4db1dc5f
Petre Carmen <elenacarmen.petre@gmail.com>
2022-06-08 10:28:25 +0300

    Add comments

--- main.js
+++ main.js
@@ -1,15 +1,13 @@
-// import fetch from 'node-fetch';
-fetch = require('node-fetch');
+fetch = require('node-fetch'); //incarc modulele in node
 atob = require('atob');
 
-// https://api.signicat.io/identification/v2/sessions
-
-const username = process.env.USERNAME;
+const username = process.env.USERNAME; //accesam variabilele de env ca sa luam username si password
 const password = process.env.PASSWORD;
-const credentials = btoa(username + ":" + password);
+const credentials = btoa(username + ":" + password); //le transf in base64 pt basic auth
+
 
 let currentToken = null;
-fetch("https://api.signicat.io/oauth/connect/token", {
+fetch("https://api.signicat.io/oauth/connect/token", { //request la url, rezultatul e un promise
     method: "POST",
     headers: {
         "Content-Type": "application/x-www-form-urlencoded",
@@ -17,12 +15,12 @@ fetch("https://api.signicat.io/oauth/connect/token", {
         "Accept": "*/*"
     },
     body: "grant_type=client_credentials"
-}).then(r => r.json())
+}).then(r => r.json()) // citesc body-ul responseului si il parsez (json -> object)
     .then(accessTokenResponse => {
-        console.log(accessTokenResponse);
+        console.log("accessTokenResponse", accessTokenResponse);
         currentToken = accessTokenResponse['access_token']
 
-        const sessionRequestBody = {
+        const sessionRequestBody = { //din postman
             "flow": "redirect",
             "allowedProviders": [
                 "no_bankid_netcentric",
@@ -39,7 +37,7 @@ fetch("https://api.signicat.io/oauth/connect/token", {
             }
         };
 
-        return fetch("https://api.signicat.io/identification/v2/sessions", {
+        return fetch("https://api.signicat.io/identification/v2/sessions", {  // cream sesiunea
             method: "POST",
             headers: {
                 'Authorization': "Bearer " + accessTokenResponse['access_token'],
@@ -47,24 +45,25 @@ fetch("https://api.signicat.io/oauth/connect/token", {
             },
             body: JSON.stringify(sessionRequestBody),
         }).then(r => {
-            return r.json().then(json => {
-                return [accessTokenResponse, json]
+            return r.json().then(session => { //citesc body, il parsez ca json (json->obj)
+                return [accessTokenResponse, session]
             });
         });
-    }).then(([accessTokenResponse, x]) => {
-    console.log(x);
-    return new Promise(resolve => {
+    }).then(([accessTokenResponse, session]) => {
+    console.log("session", session);
+    return new Promise(resolve => { //return promise care peste 60s trimite acesstokenresp, session
         setTimeout(() => {
-            resolve([accessTokenResponse, x])
+            resolve([accessTokenResponse, session])
         }, 60000)
     })
-}).then(([accessTokenResponse, x]) => {
-        fetch(`https://api.signicat.io/identification/v2/sessions/${x.id}`, {
+}).then(([accessTokenResponse, session]) => { //check daca s-a auth cu ID-ul
+        fetch(`https://api.signicat.io/identification/v2/sessions/${session.id}`, {
             method: 'GET',
             headers: {
                 'Authorization': "Bearer " + accessTokenResponse['access_token'],
             }
         }).then(r => r.json()).then(responseBody => console.log("responseBody", responseBody))
+            .catch(err => console.log("no auth on this session yet", err));
     }
-).catch(err => console.log("no auth on this session yet", err));
+);
 
